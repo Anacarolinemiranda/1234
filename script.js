3d4f5fae3243759e9d6fbedfc04e298ec331eccf
ana caroline miranda <143092285+Anacarolinemiranda@users.noreply.github.com>
2025-09-19 08:05:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Pequena animação de rolagem suave
+document.querySelectorAll("a[href^='#']").forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
